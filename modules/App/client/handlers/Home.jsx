01b0793883e14b8d2be93c005d85ed59b9244eac
Theodor Vararu <theo@vararu.org>
2015-11-04 20:04:29 +0000

    Refactor MUI boilerplate to Root.jsx component.

--- modules/App/client/handlers/Home.jsx
+++ modules/App/client/handlers/Home.jsx
@@ -1,32 +1,9 @@
-import { Component, PropTypes } from 'react'
-
-import { AppCanvas, AppBar, Styles, RaisedButton, DatePicker } from 'material-ui'
-const { ThemeManager, LightRawTheme } = Styles
+import { Component } from 'react'
 
 export default class Home extends Component {
-  static childContextTypes = {
-    muiTheme: PropTypes.object
-  }
-
-  getChildContext () {
-    return {
-      muiTheme: ThemeManager.getMuiTheme(LightRawTheme)
-    }
-  }
-
   render () {
-    return <AppCanvas>
-      <AppBar title='Hello'/>
-      <div style={{padding: '80px'}}>
-        <RaisedButton primary={true} label='Tap' />
-        <br/>
-        <DatePicker hintText='Portrait Dialog' />
-        <br/>
-        <DatePicker
-          hintText='Landscape Dialog'
-          mode='landscape'
-        />
-      </div>
-    </AppCanvas>
+    return <div>
+      Hello
+    </div>
   }
 }
--- modules/App/client/handlers/Root.jsx
+++ modules/App/client/handlers/Root.jsx
@@ -1,11 +1,24 @@
 import { Component, PropTypes } from 'react'
 
+import { AppCanvas, Styles } from 'material-ui'
+const { ThemeManager, LightRawTheme } = Styles
+
 export default class Root extends Component {
   static propTypes = {
     children: PropTypes.element
   }
 
+  static childContextTypes = {
+    muiTheme: PropTypes.object
+  }
+
+  getChildContext () {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(LightRawTheme)
+    }
+  }
+
   render () {
-    return <div>{ this.props.children }</div>
+    return <AppCanvas>{ this.props.children }</AppCanvas>
   }
 }
