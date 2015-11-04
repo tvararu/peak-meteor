8951a91d581677cbb06c6fadeaca94dceeb299de
Theodor Vararu <theo@vararu.org>
2015-11-04 19:29:12 +0000

    Scaffold initial material-ui test component.

--- modules/App/client/handlers/Home.jsx
+++ modules/App/client/handlers/Home.jsx
@@ -1,7 +1,32 @@
-import { Component } from 'react'
+import { Component, PropTypes } from 'react'
+
+import { AppCanvas, AppBar, Styles, RaisedButton, DatePicker } from 'material-ui'
+const { ThemeManager, LightRawTheme } = Styles
 
 export default class Home extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object
+  }
+
+  getChildContext () {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(LightRawTheme)
+    }
+  }
+
   render () {
-    return <div>Hello world!</div>
+    return <AppCanvas>
+      <AppBar title='Hello'/>
+      <div style={{padding: '80px'}}>
+        <RaisedButton primary={true} label='Tap' />
+        <br/>
+        <DatePicker hintText='Portrait Dialog' />
+        <br/>
+        <DatePicker
+          hintText='Landscape Dialog'
+          mode='landscape'
+        />
+      </div>
+    </AppCanvas>
   }
 }
