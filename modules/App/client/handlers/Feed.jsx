52d8d14e75e96a4b5973fcc53fe7aee0e9cfda45
Theodor Vararu <theo@vararu.org>
2015-11-17 20:43:39 +0000

    Use material-ui cards as the FeedItem components.

--- modules/App/client/handlers/Feed.jsx
+++ modules/App/client/handlers/Feed.jsx
@@ -1,7 +1,55 @@
 import { Component } from 'react'
+import _ from 'lodash'
+import Radium from 'radium'
 
-export default class Feed extends Component {
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardText,
+  CardTitle,
+  FlatButton
+} from 'material-ui'
+
+@Radium
+class FeedItem extends Component {
+  getStyles () {
+    return {
+      marginBottom: '10px'
+    }
+  }
+
   render () {
-    return <div>Feed.</div>
+    return <Card style={ this.getStyles() }>
+      <CardHeader
+        avatar={ <Avatar>A</Avatar> }
+        subtitle='Subtitle'
+        title='Title'
+      />
+      <CardText>
+        Lorem ipsum.
+      </CardText>
+    </Card>
+  }
+}
+
+@Radium
+export default class Feed extends Component {
+  getStyles () {
+    return {
+      padding: '10px'
+    }
+  }
+
+  render () {
+    const feedItems = _.range(0, 10)
+
+    return <div style={ this.getStyles() }>
+      <div>
+        { feedItems.map(n => <FeedItem key={ n } />) }
+      </div>
+    </div>
   }
 }
