2cc32f970ed0a0431c01e3d53d0cc3cff9a52a5a
Theodor Vararu <theo@vararu.org>
2015-12-20 17:49:16 +0000

    Update to new versions from kickstart-simple.

--- entry/client/webpack.conf.js
+++ entry/client/webpack.conf.js
@@ -1,21 +1,28 @@
-var babelSettings = { stage: 0 }
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-if (process.env.NODE_ENV !== 'production') {
-  babelSettings.plugins = ['react-transform']
-  babelSettings.extra = {
-    'react-transform': {
-      transforms: [{
-        transform: 'react-transform-hmr',
-        imports: ['react'],
-        locals: ['module']
-      }, {
-        transform: 'react-transform-catch-errors',
-        imports: ['react', 'redbox-react']
-      }]
-      // redbox-react is breaking the line numbers :-(
-      // you might want to disable it
-    }
-  }
+var babelSettings = { presets: ['react', 'es2015', 'stage-0'] }
+babelSettings.plugins = ['transform-decorators-legacy']
+
+var cssLoader
+var plugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new ExtractTextPlugin('style.css'))
+  cssLoader = ExtractTextPlugin.extract('style', 'css?module&localIdentName=[hash:base64:5]')
+} else {
+  cssLoader = 'style!css?module&localIdentName=[name]__[local]__[hash:base64:5]'
+  babelSettings.plugins.push(['react-transform', {
+    transforms: [{
+      transform: 'react-transform-hmr',
+      imports: ['react'],
+      locals: ['module']
+    }, {
+      transform: 'react-transform-catch-errors',
+      imports: ['react', 'redbox-react']
+    }]
+    // redbox-react is breaking the line numbers :-(
+    // you might want to disable it
+  }])
 }
 
 module.exports = {
@@ -23,9 +30,10 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, loader: 'babel', query: babelSettings, exclude: /node_modules/ },
-      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.css$/, loader: cssLoader },
       { test: /\.(png|jpe?g)(\?.*)?$/, loader: 'url?limit=8182' },
       { test: /\.(svg|ttf|woff|eot)(\?.*)?$/, loader: 'file' }
     ]
-  }
+  },
+  plugins: plugins
 }
