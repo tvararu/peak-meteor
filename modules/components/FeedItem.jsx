8440cb337be374625172a3e9f41294939b940ef8
Theodor Vararu <theo@vararu.org>
2016-01-21 23:04:54 +0000

    Switch FeedItem.jsx to react-pure-render.

--- modules/components/FeedItem.jsx
+++ modules/components/FeedItem.jsx
@@ -1,6 +1,6 @@
+// @flow
 import React, { Component } from 'react'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
-import reactMixin from 'react-mixin'
+import shouldPureComponentUpdate from 'react-pure-render'
 import gravatar from 'gravatar'
 import PropTypes from 'lib/PropTypes'
 import {
@@ -9,13 +9,14 @@ import {
   CardText
 } from 'mui'
 
-@reactMixin.decorate(PureRenderMixin)
 export default class FeedItem extends Component {
   static propTypes = {
     author: PropTypes.pk.user.isRequired,
     createdAt: PropTypes.instanceOf(Date).isRequired,
     text: PropTypes.string.isRequired
   };
+
+  shouldComponentUpdate = shouldPureComponentUpdate;
 
   render () {
     const email = this.props.author.emails[0].address
