e45098d663862b77ea17c441d6ae306ed7efbdcc
Theodor Vararu <theo@vararu.org>
2016-04-27 22:52:52 +0100

    Add text fields to profile page.

--- src/handlers/Profile.jsx
+++ src/handlers/Profile.jsx
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
 import Radium from 'radium'
+import gravatar from 'gravatar'
+import {
+  Card,
+  CardHeader,
+  CardText,
+  RaisedButton,
+  TextField
+} from 'material-ui'
 
 class Profile extends Component {
+  getStyles () {
+    return {
+      margin: '0 auto',
+      maxWidth: '600px',
+      padding: '0 10px 10px 10px'
+    }
+  }
+
   render () {
-    return <div>
-      Hello world!
+    const email = 'theo@vararu.org'
+    const avatarUrl = gravatar.imageUrl(email)
+
+    return <div style={ this.getStyles() }>
+      <h1>Profile</h1>
+      <Card style={{ paddingBottom: '16px' }}>
+        <CardHeader
+          avatar={ avatarUrl }
+          subtitle={ <TextField
+            hintText='Title and Company'
+            style={{
+              fontSize: '13px',
+              lineHeight: '14px',
+              height: '38px'
+            }}
+          /> }
+          title={ email }
+        />
+        <CardText style={{ clear: 'both' }}>
+          <br />
+          <TextField
+            hintText='Enter a brief description about yourself.'
+            multiLine={ true }
+          /><br />
+          <RaisedButton
+            label='Save'
+            primary={ true }
+            style={{ float: 'right' }}
+          />
+        </CardText>
+      </Card>
     </div>
   }
 }
