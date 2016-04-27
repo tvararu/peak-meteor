9e896bf58353aac6073646cc2c2022e9507fb61f
Theodor Vararu <theo@vararu.org>
2016-04-27 23:55:07 +0100

    Implement profile saving.

--- src/handlers/Profile.jsx
+++ src/handlers/Profile.jsx
@@ -1,6 +1,11 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
+import Meteor from 'meteor'
 import Radium from 'radium'
 import gravatar from 'gravatar'
+import reactMixin from 'react-mixin'
+import ReactMeteorData from 'meteor/ReactMeteorData'
+import PropTypes from 'lib/PropTypes'
 import {
   Card,
   CardHeader,
@@ -9,7 +14,71 @@ import {
   TextField
 } from 'material-ui'
 
+@reactMixin.decorate(ReactMeteorData)
+class ProfileHandler extends Component {
+  constructor (props) {
+    super(props)
+
+    _.bindAll(
+      this,
+      'save'
+    )
+  }
+
+  getMeteorData () {
+    const user = Meteor.user()
+    return { user }
+  }
+
+  save (title = '', description = '') {
+    Meteor.users.update(
+      this.data.user._id,
+      {$set: {
+        'profile.title': title,
+        'profile.description': description
+      }}
+    )
+  }
+
+  render () {
+    const email = this.data.user.emails[0].address
+    const avatarUrl = gravatar.imageUrl(email)
+    let description = ''
+    let title = ''
+    if (this.data.user && this.data.user.profile) {
+      description = this.data.user.profile.description
+      title = this.data.user.profile.title
+    }
+
+    return <Profile
+      avatarUrl={ avatarUrl }
+      description={ description }
+      email={ email }
+      title={ title }
+      onSave={ this.save }
+    />
+  }
+}
+
 class Profile extends Component {
+  static propTypes = {
+    avatarUrl: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    email: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    onSave: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    title: '',
+    description: ''
+  };
+
+  state = {
+    title: this.props.title,
+    description: this.props.description
+  };
+
   getStyles () {
     return {
       margin: '0 auto',
@@ -18,9 +87,35 @@ class Profile extends Component {
     }
   }
 
+  constructor (props) {
+    super(props)
+
+    _.bindAll(
+      this,
+      'handleTitleChange',
+      'handleDescriptionChange'
+    )
+  }
+
+  handleTitleChange (title) {
+    this.setState({title})
+  }
+
+  handleDescriptionChange (description) {
+    this.setState({description})
+  }
+
   render () {
     const email = 'theo@vararu.org'
     const avatarUrl = gravatar.imageUrl(email)
+    const valueLinkTitle = {
+      value: this.state.title,
+      requestChange: this.handleTitleChange
+    }
+    const valueLinkDescription = {
+      value: this.state.description,
+      requestChange: this.handleDescriptionChange
+    }
 
     return <div style={ this.getStyles() }>
       <h1>Profile</h1>
@@ -34,6 +129,7 @@ class Profile extends Component {
               lineHeight: '14px',
               height: '38px'
             }}
+            valueLink={ valueLinkTitle }
           /> }
           title={ email }
         />
@@ -42,11 +138,18 @@ class Profile extends Component {
           <TextField
             hintText='Enter a brief description about yourself.'
             multiLine={ true }
+            valueLink={ valueLinkDescription }
           /><br />
           <RaisedButton
             label='Save'
             primary={ true }
             style={{ float: 'right' }}
+            onClick={ () => {
+              this.props.onSave(
+                this.state.title,
+                this.state.description
+              )
+            } }
           />
         </CardText>
       </Card>
@@ -54,4 +157,4 @@ class Profile extends Component {
   }
 }
 
-export default Radium(Profile)
+export default Radium(ProfileHandler)
