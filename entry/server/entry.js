8de82d39c9a7fefb34a382387b2929cfd41a4fa3
Theodor Vararu <theo@vararu.org>
2016-06-12 19:28:53 +0100

    Disable server side rendering.

--- entry/server/entry.js
+++ entry/server/entry.js
@@ -1,12 +1,12 @@
-import ReactRouterSSR from 'meteor/ReactRouterSSR'
-import WebpackStats from 'meteor/WebpackStats'
+// import ReactRouterSSR from 'meteor/ReactRouterSSR'
+// import WebpackStats from 'meteor/WebpackStats'
 import 'lib/methods'
 import 'server/publications'
 
-// Do server-rendering only in production mode
-if (process.env.NODE_ENV === 'production') {
-  // Load Webpack infos for SSR.
-  ReactRouterSSR.LoadWebpackStats(WebpackStats)
-
-  require('../client/routes')
-}
+// // Do server-rendering only in production mode
+// if (process.env.NODE_ENV === 'production') {
+//   // Load Webpack infos for SSR.
+//   ReactRouterSSR.LoadWebpackStats(WebpackStats)
+//
+//   require('../client/routes')
+// }
